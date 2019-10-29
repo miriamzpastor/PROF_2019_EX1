79a2d6b1fed4e4d30239c9ab5eab533409cd539c
miriamzpastor <miriam.zpastor@alumnos.upm.es>
2019-10-29 16:07:00 +0100

    Código

--- src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
@@ -3,20 +3,24 @@ package es.upm.grise.profundizacion2019.ex1;
 public class ClaseExamen {
 	
 	public float metodoAProbar(int a, int b) {
-		
+		//nodo 1
 		float result = 0;
-		
+		//nodo 2
 		while( a > b) {
+			//nodo 3
 			a--;
 		}
-		
+		//nodo 4
 		if (a <= b) {
+			//nodo 5
 			result = 0;
 		}
+		//nodo 6
+		//no entrará nunca aquí
 		else {
 			result = b;
 		}
-		
+		//nodo 
 		return result;
 	}
 
--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ClaseExamenTest {
 
+public class ClaseExamenTest {
+	ClaseExamen prueba =new ClaseExamen();
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(0,prueba.metodoAProbar(8,7),0);
 	}
-
+	@Test
+	public void test2() {
+		assertEquals(0,prueba.metodoAProbar(3,4),0);
+	}
 }
